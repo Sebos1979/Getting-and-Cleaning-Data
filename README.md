4887af2e7fa6e7b21b765f8edbb58048bec18e6b
Sebos1979 <sebastien.nicault@gmail.com>
2021-02-04 03:34:51 +0100

    First commit - project file, script, data

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Getting-and-Cleaning-Data
 Repository for Coursera JHU Getting and Cleaning Data Course Project
+
+# 1 - LOAD packages ----
+
+# 2 - LOAD data ----
+## 2.1 - Download the HUCI HAR dataset from url provided, unzip in data folder
+## 2.2 - Delete compressed file
+## 2.3 - get test data : subjects, labels and dataset from test folder----
+## 2.4 - get train data : subjects, labels and dataset from train folder----
+## 2.5 - get features list and activity labels data ----
+
+# 3 - CLEAN and PREPARE data ----
+
+## 3.1 - preparing feature data set ----
+## 3.1.1 - getting features labels into a vector
+## 3.1.2 - applying features labels to test and train datasets
+## 3.1.3 - subsetting mean and std features only
+# 3.1.3.1 - getting a vector of mean measurements variables - contains only "mean()"
+# 3.1.3.2 - getting a vector of standard deviation measurements variables - contains only "std()"
+# 3.1.3.3 - Binding mean & std vars in a single vector
+## 3.1.4 - keeping only mean and std column on test & train data set
+
+## 3.2 - preparing activity data set ----
+## 3.2.1 - adding labels to columns (primary key activity_id)
+## 3.2.2 - adding labels to column activity
+## 3.2.3 - adding activity names to test & train labels tables
+## 3.2.4 - Setting descriptive label for the subject in subject datasets
+
+## 3.3 - binding test & train data sets ----
+# 3.3.1 - binding subject, labels and dataset into test data set
+# 3.3.2 - binding subject, labels and dataset into train data set
+
+# 4 - VISUALISE tidy data ----
+
+## 4.1 - Tidy data number 1: data set with descriptive variable names ----
+### 4.1.1 - Binding test and train datasets
+### 4.1.2 - (optional) deleting all the objects in memory except the resulting data set
+### 4.1.3 - writing the output file named HAR_set in the output folder
+
+## 4.2 - Tidy data number 2: data set with the average of each variable for each activity and each subject. ----
+### 4.2.1 - converting the subjectId colmun to a character class
+### 4.2.2 - applying mean to all mean and standard deviation variables and grouping by subject and activity
+### 4.2.3 - writing the output file named HAR_gp_avg in the output folder
+### 4.2.4 - deleting all objects in memory
+   
+   
---
+++ run_analysis.R
@@ -0,0 +1,120 @@
+
+# 1 - LOAD packages ----
+
+library(dplyr)
+
+# 2 - LOAD data ----
+
+## 2.1 - Download the dataset from url, unzip in /data/ rep
+zip_ds <- "data.zip"
+
+if (!file.exists(zip_ds)){
+            fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+            download.file(fileURL, zip_ds, method="curl")
+}  
+if (!file.exists("data")) { 
+            unzip(zipfile = zip_ds, exdir = "./data/") 
+}
+
+
+## 2.2 - Delete compressed file
+if (file.exists(zip_ds)) {
+            file.remove(zip_ds)
+}
+
+
+## 2.3 - get test data ----
+test_subject <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+test_set <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+test_labels <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+
+## 2.4 - get train data ----
+train_subject <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+train_set <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+train_labels <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+
+## 2.5 - get features list and activity labels data ----
+features <- read.table("./data/UCI HAR Dataset/features.txt")
+activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+
+# 3 - CLEAN and PREPARE data ----
+
+## 3.1 - preparing feature data set ----
+
+## getting features labels 
+features_labels <- as.vector(features$V2)
+
+## applying features labels to data sets 
+names(test_set) = features_labels
+names(train_set) = features_labels
+
+## subsetting mean and std features only
+
+# getting a vector of mean measurements variables - contains only "mean()"
+mean_feat <- print(grep("mean\\(\\)", features$V2, value = TRUE))
+
+# getting a vector of standard deviation measurements variables - contains only "std()"
+std_feat <- print(grep("std\\(\\)", features$V2, value = TRUE))
+
+# combine mean & std vars in a single vector
+vars_features <- c(mean_feat, std_feat)
+
+## keeping only mean and std column on test & train data set (using dplyr)
+test_set <- test_set %>%
+            select(all_of(vars_features))
+train_set <- train_set %>%
+            select(all_of(vars_features))
+
+## 3.2 - preparing activity data set ----
+
+## adding labels to columns (primary key activity_id)
+names(activity_labels)[names(activity_labels) == "V1"] = "activityId"
+names(test_labels)[names(test_labels) == "V1"] = "activityId"
+names(train_labels)[names(train_labels) == "V1"] = "activityId"
+
+## adding labels to column activity
+names(activity_labels)[names(activity_labels) == "V2"] = "activity"
+
+## merge activity names with test & train labels tables
+test_labels <- inner_join(test_labels, activity_labels)
+train_labels <- inner_join(train_labels, activity_labels)
+
+## preparing subject data set
+names(test_subject)[names(test_subject) =="V1"] = "subjectId"
+names(train_subject)[names(train_subject) =="V1"] = "subjectId"
+
+## 3.3 - binding test & train data sets ----
+
+# binding columns for test data set
+test <- cbind(test_subject, test_labels, test_set)
+
+# binding columns for train data set
+train <- cbind(train_subject, train_labels, train_set)
+
+# 4 - VISUALISE tidy data ----
+
+## 4.1 - Tidy data #1: data set with descriptive variable names ----
+HAR_set <- rbind(train, test)
+HAR_set <- HAR_set[,-2]
+
+# deleting all the objects in memory except the resulting data set
+rm(list=(ls()[ls()!="HAR_set"]))
+
+# writing the output in a file for answer 1
+write.table(HAR_set,"./output/HAR_set.txt")
+
+
+## 4.2 - Tidy data #2: data set with the average of each variable for each activity and each subject. ----
+
+HAR_set$subjectId <- as.character(HAR_set$subjectId)
+
+HAR_gp_avg <- aggregate(HAR_set[,3:ncol(HAR_set)], 
+                        by=list("Subject" = HAR_set$subjectId, "Activity" = HAR_set$activity), FUN= mean)
+
+# writing the output in a file for answer 2
+write.table(HAR_gp_avg,"./output/HAR_gp_avg.txt")
+
+# deleting all objects in memory
+rm(list=ls())
+   
+   
